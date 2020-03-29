654389ec35e98cfd7f414089148313bbf741225f
unknown <279290309@qq.com>
2020-03-29 18:04:17 +0800

    初始化OA起始环境

---
+++ src/main/java/cn/xyz/yzoa/YzoaApplication.java
@@ -0,0 +1,13 @@
+package cn.xyz.yzoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xyz/yzoa/YzoaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xyz.yzoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
